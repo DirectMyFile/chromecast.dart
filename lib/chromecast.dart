a9f51760dd9faf0c6ed6f262ab7db85526dffd3d
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-03 16:11:36 -0500

    Still Broken

--- lib/chromecast.dart
+++ lib/chromecast.dart
@@ -112,18 +112,10 @@ class Chromecast {
   }
 
   void sendBuffer(Uint8List data) {
-    var bytes = data.buffer.asUint32List();
+    var bytes = data.buffer.asUint8List();
     
-    _socket.add((new ByteData(4)..setUint32(0, bytes.lengthInBytes, Endianness.BIG_ENDIAN)).buffer.asUint32List());
-    
-    var buff = new ByteData(bytes.lengthInBytes);
-    
-    int offset = 0;
-    for (var byte in bytes) {
-      buff.setUint32(offset, byte, Endianness.BIG_ENDIAN);
-      offset += 4;
-    }
-    _socket.add(buff.buffer.asUint32List());
+    var header = (new ByteData(4)..setUint32(0, bytes.lengthInBytes, Endianness.BIG_ENDIAN)).buffer.asUint32List();
+    _socket.add([]..addAll(header)..addAll(bytes));
   }
 }
 
